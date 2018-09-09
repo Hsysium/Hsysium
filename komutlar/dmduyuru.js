e13bcfcd5b19977568b062b3d8f0746d99f2d12a
akınweeghu <43097435+Hsysium@users.noreply.github.com>
2018-09-09 15:38:50 +0300

    Update dmduyuru.js

--- komutlar/dmduyuru.js
+++ komutlar/dmduyuru.js
@@ -2,9 +2,9 @@ const Discord = require('discord.js');
 const ayarlar = require('../ayarlar.json');
 
 exports.run = (client, message, args) => {
-if (message.author.id !=469094615761420299 ) { return; }
+if (message.author.id !=320842708690272267 ) { return; }
   let mesaj = args.slice(0).join(' ');
-if (mesaj.length < 1) return message.channel.send('Birþey Yazmalýsýnýz');
+if (mesaj.length < 1) return message.channel.send('BirÃ¾ey YazmalÃ½sÃ½nÃ½z');
   message.delete();
       const mesajat = new Discord.RichEmbed()
       .setColor('RANDOM')
@@ -24,6 +24,6 @@ exports.conf = {
 
 exports.help = {
   name: 'dmduyuru',
-  description: 'Ýstediðiniz þeyi bota duyurtur.',
-  usage: 'duyuru [duyurmak istediðiniz þey]'
+  description: 'ÃstediÃ°iniz Ã¾eyi bota duyurtur.',
+  usage: 'duyuru [duyurmak istediÃ°iniz Ã¾ey]'
 };
